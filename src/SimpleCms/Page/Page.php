650ec586146ea253ee28e9c8a1879132e9f29cac
Tom Green <kingofcuddles@gmail.com>
2014-10-27 18:25:16 +0000

    dropped asset reg.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/SimpleCms/Page/Page.php
+++ src/SimpleCms/Page/Page.php
@@ -1,12 +1,8 @@
 <?php namespace SimpleCms\Page;
 
 use SimpleCms\Core\BaseModel;
-use SimpleCms\Asset\ModelTrait;
 
 class Page extends BaseModel {
-
-  // Include our Asset Trait
-  use ModelTrait;
 
   protected $fillable = [
     'status',
