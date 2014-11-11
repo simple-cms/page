eb63ba7ae01f9f3bd5726ca785eca05f20426236
Tom Green <kingofcuddles@gmail.com>
2014-11-11 18:17:43 +0000

    working on base templates.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/views/Public/show.blade.php
+++ src/views/Public/show.blade.php
@@ -1,13 +1,18 @@
 @extends('core::Public/Base')
 
 @section('content')
-  <h2>{{ $page->title }}</h2>
-  <p><small>Posted on {{ $page->created_at }}
-    @if ($page->updated_at != '0000-00-00 00:00:00')
-    (last updated at {{ $page->updated_at }})
-    @endif
-  </small></p>
-  <blockquote>{{ $page->excerpt }}</blockquote>
-  {!! $page->content !!}
-  <hr />
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>{{ $page->title }}</h2>
+      <p><small>Posted on {{ $page->created_at }}
+        @if ($page->updated_at != '0000-00-00 00:00:00')
+        (last updated at {{ $page->updated_at }})
+        @endif
+      </small></p>
+      <blockquote>{{ $page->excerpt }}</blockquote>
+      {!! $page->content !!}
+    </div>
+  </div>
+</div>
 @stop
