8115ea32dc39a712977f9fb17f8316f4e3164368
Tom Green <kingofcuddles@gmail.com>
2014-12-29 11:13:05 +0000

    config update.
    Signed-off-by: Tom Green <kingofcuddles@gmail.com>

--- src/config/config.php
+++ src/config/config.php
@@ -4,4 +4,34 @@ return [
   // Admin Page URL - example.com/<adminURL>/<pageAdminURL>
   'pageURL' => 'page',
 
+  // Our media file configuration
+  'mediaConfiguration' => [
+    // The directory (relative to public/media) to store these files
+    'path' => 'page',
+
+    // An array of image sizes to generate
+    'sizes' => [
+      'original' => [
+        'width' => null,
+        'height' => null,
+        'quality' => 100,
+        'crop' => false,
+        'maintainRatio' => true
+      ],
+      'thumb' => [
+        'width' => 250,
+        'height' => 250,
+        'quality' => 70,
+        'crop' => false,
+        'maintainRatio' => true
+      ],
+      'main' => [
+        'width' => 500,
+        'height' => 350,
+        'quality' => 70,
+        'crop' => false,
+        'maintainRatio' => true
+      ]
+    ],
+  ]
 ];
